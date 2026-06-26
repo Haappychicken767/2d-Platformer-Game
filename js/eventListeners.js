3058d641079c49be3f0418e9a51db69638ad2a80
Applekie12 <anishrk369@gmail.com>
2026-06-26 12:36:00 -0700

    updated index.js, created eventListeners.js
    Created the movement features and jump key using eventListeners.

---
+++ js/eventListeners.js
@@ -0,0 +1,34 @@
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    // Player jump
+    case "w":
+      if (player.velocity.y === 0) {
+        player.velocity.y = -20;
+      }
+      break;
+
+    // Move player to left
+    case "a":
+      keys.a.pressed = true;
+      break;
+
+    // Move player to right
+    case "d":
+      keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    // Move player to left
+    case "a":
+      keys.a.pressed = false;
+      break;
+
+    // Move player to right
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});
