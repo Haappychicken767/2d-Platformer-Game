cb8b3de5b249d17d4aca63a3ef7697620da5065c
Applekie12 <anishrk369@gmail.com>
2026-07-09 19:28:54 -0700

    updated index.js and Player.js
    Added horizontal collision detection in Player.js, that used the Collision Block attributes and Player attributes to correct Player x position and implement the collision

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,5 +1,5 @@
 class Player {
-  constructor() {
+  constructor({ collisionBlocks = [] }) {
     this.position = {
       x: 100,
       y: 100,
@@ -16,6 +16,8 @@ class Player {
       bottom: this.position.y + this.height,
     };
     this.gravity = 1;
+
+    this.collisionBlocks = collisionBlocks;
   }
 
   draw() {
@@ -25,6 +27,28 @@ class Player {
 
   update() {
     this.position.x += this.velocity.x;
+    //horizontal collisions checking
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        if (this.velocity.x < -1) {
+          this.position.x =
+            collisionBlock.position.x + collisionBlock.width + 0.01;
+          break;
+        }
+
+        if (this.velocity.x > 1) {
+          this.position.x = collisionBlock.position.x - this.width - 0.01;
+          break;
+        }
+      }
+    }
+
     this.position.y += this.velocity.y;
     this.sides.bottom = this.position.y + this.height;
 
